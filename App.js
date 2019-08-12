fcd6554806478a6a59f7e309ce00c2d6d6425ebb
WoLewicki <lewywojt@gmail.com>
2019-08-12 11:39:01 +0200

    hooks version of App

--- App.js
+++ App.js
@@ -1,28 +1,30 @@
-import React from "react";
+import React, { useState } from 'react';
 import { ScrollView, Platform, Button, Image } from "react-native";
 import FastImage from "react-native-fast-image";
 
 const img1 = require('./bulbi.svg');
 const img2 = require('./charmi.svg');
 
-export default class App extends React.Component {
-  state = {
-    size: 100,
-    key: 1
+export default props => {
+  const [size, setSize] = useState(100);
+  const [key, setKey] = useState(1); // key used to show the scalability in FastImage
+  const Enlarger = () => <Button // Used to show the scalability
+  onPress={() => 
+    {
+    setSize(size*1.5);
+    setKey(key+1); 
+    }
   }
-  
-  render(){
-    Enlarger = () => <Button // Used to show the scalability
-  onPress={() => this.setState({ size: this.state.size * 1.5 , key: this.state.key + 1})} // key used to show the scalability in FastImage
   title="Enlarge"
     />;
+
     if(Platform.OS === 'android'){
       return (
         <ScrollView>
           <Enlarger />
-          <FastImage style={{width: this.state.size, height: this.state.size}} key={this.state.key} source={img2}/>
-          <FastImage style={{width: this.state.size, height: this.state.size}} source={img1}/>
-          <FastImage style={{width: this.state.size, height: this.state.size}} source={img2}/>
+          <FastImage style={{width: size, height: size}} key={key} source={img2}/>
+          <FastImage style={{width: size, height: size}} source={img1}/>
+          <FastImage style={{width: size, height: size}} source={img2}/>
         </ScrollView>
       );
     }
@@ -30,10 +32,9 @@ export default class App extends React.Component {
         return(
           <ScrollView style={{paddingTop: 40}}>
             <Enlarger />
-            <Image style={{width: this.state.size, height: this.state.size}} source={img1} />
-            <Image style={{width: this.state.size, height: this.state.size}} source={img2} />
+            <Image style={{width: size, height: size}} source={img1} />
+            <Image style={{width: size, height: size}} source={img2} />
           </ScrollView>
         )
     }
-  }
 }
